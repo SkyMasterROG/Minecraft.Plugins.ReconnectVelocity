ae34a11847c2d1f790442d1b5dc907839ed17aef
sB3NZ1N63r <steffenbenzinger@msn.com>
2024-04-10 16:39:55 +0200

    add BuildConstants.java

---
+++ src/main/templates/com/mattmx/reconnect/BuildConstants.java
@@ -0,0 +1,10 @@
+package com.mattmx.reconnect;
+
+// The constants are replaced before compilation
+public class BuildConstants {
+    
+    public static final String VERSION = "${version}";
+    public static final String BASE_NAME = "${base_name}";
+    public static final String PROJ_NAME = "${proj_name}";
+    public static final String PROJ_DESC = "${description}";
+}
